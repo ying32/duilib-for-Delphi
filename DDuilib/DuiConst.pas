2f0f11efc4553f8e3214e26865600fcf596b02b9
ying32 <zying32@163.com>
2015-11-29 01:21:56 +0800

    DuiConst.pas忘了提交。。。

---
+++ DDuilib/DuiConst.pas
@@ -0,0 +1,39 @@
+//***************************************************************************
+//
+//       名称：DuiConst.pas
+//       工具：RAD Studio XE6
+//       日期：2015/11/29 0:28:12
+//       作者：ying32
+//       QQ  ：1444386932
+//       E-mail：1444386932@qq.com
+//       版权所有 (C) 2015-2015 ying32 All Rights Reserved
+//
+//
+//***************************************************************************
+unit DuiConst;
+
+interface
+
+const
+
+  DUI_EVENT_WINDOWINIT = 'windowinit';
+  DUI_EVENT_CLICK = 'click';
+  DUI_EVENT_RETURN = 'return';
+  DUI_EVENT_KILLFOCUS = 'killfocus';
+
+  // CListUI
+  DUI_EVENT_ITEMSELECT = 'itemselect';
+  DUI_EVENT_ITEMACTIVATE = 'itemactivate';
+  DUI_EVENT_ITEMCLICK = 'itemclick';
+  DUI_EVENT_SELECTCHANGED = 'selectchanged';
+
+  // CSliderUI
+  DUI_EVENT_VALUECHANGED = 'valuechanged';
+
+
+
+  DUI_CLASS_ListContainerElementUI = 'ListContainerElementUI';
+
+implementation
+
+end.
