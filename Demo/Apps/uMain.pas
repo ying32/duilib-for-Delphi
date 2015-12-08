a8ba63a6d26536757ff6986cf6bc346bae686d7a
ying32 <zying32@163.com>
2015-12-08 15:59:29 +0800

    更新demo apps

--- Demo/Apps/uMain.pas
+++ Demo/Apps/uMain.pas
@@ -21,6 +21,9 @@ const
 type
 
   TAppsWindow = class(TDuiWindowImplBase)
+  private
+    FDlgBuilder: CDialogBuilder;
+    FInitOK: Boolean;
   protected
     procedure DoNotify(var Msg: TNotifyUI); override;
     procedure DoHandleMessage(var Msg: TMessage; var bHandled: BOOL); override;
@@ -38,15 +41,31 @@ implementation
 
 { TAppsWindow }
 
+{
+  if not FDlgBuilder.GetMarkup.IsValid then
+    LListElement := CListContainerElementUI(FDlgBuilder.Create('iconslist.xml', '', nil, FPaintManager))
+  else
+    LListElement := CListContainerElementUI(FDlgBuilder.Create(nil, FPaintManager));
+  if LListElement = nil then
+    Exit;
+
+}
+
 constructor TAppsWindow.Create;
 begin
   inherited Create('MainWindow.xml', 'skin\Apps');
   CreateWindow(0, 'Apps', UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE);
+  FDlgBuilder := CDialogBuilder.CppCreate;
 end;
 
 destructor TAppsWindow.Destroy;
+var
+  LTileLayout: CTileLayoutUI;
 begin
-
+  LTileLayout := CTileLayoutUI(FindControl('listctrl'));
+  if LTileLayout <> nil then
+    LTileLayout.RemoveAll;
+  FDlgBuilder.CppDestroy;
   inherited;
 end;
 
@@ -56,27 +75,64 @@ begin
 end;
 
 procedure TAppsWindow.DoHandleMessage(var Msg: TMessage; var bHandled: BOOL);
+var
+  LTileLayout: CTileLayoutUI;
+  R: TRect;
 begin
   inherited;
-
+  if Msg.Msg = WM_SIZE then
+  begin
+    if FInitOK then
+    begin
+      LTileLayout := CTileLayoutUI(FindControl('listctrl'));
+      if LTileLayout <> nil then
+      begin
+        GetClientRect(Handle, R);
+        LTileLayout.SetColumns(R.Width div 90);
+        // item的宽为80 + 20的
+      end;
+    end;
+  end;
 end;
 
 procedure TAppsWindow.DoInitWindow;
-var
-  LSize: TSize;
 begin
   inherited;
-  LSize := InitSize;
-//  MoveWindow(Handle, )
 end;
 
 procedure TAppsWindow.DoNotify(var Msg: TNotifyUI);
 var
   LType, LCtlName: string;
+  LTileLayout: CTileLayoutUI;
+  LVerticalLayout: CVerticalLayoutUI;
+  I: Integer;
+  LTitle: CLabelUI;
 begin
   inherited;
   LType := Msg.sType;
   LCtlName := Msg.pSender.Name;
+  if LType.Equals(DUI_EVENT_WINDOWINIT) then
+  begin
+    FInitOK := True;
+    LTileLayout := CTileLayoutUI(FindControl('listctrl'));
+    if LTileLayout <> nil then
+    begin
+      for I := 1 to 10 do
+      begin
+        if not FDlgBuilder.GetMarkup.IsValid then
+          LVerticalLayout := CVerticalLayoutUI(FDlgBuilder.Create('buttonitem.xml', '', nil, PaintManagerUI))
+        else
+          LVerticalLayout := CVerticalLayoutUI(FDlgBuilder.Create(nil, PaintManagerUI));
+        if LVerticalLayout = nil then Continue;
+        LTitle := CLabelUI(PaintManagerUI.FindSubControlByName(LVerticalLayout, 'title'));
+        if LTitle <> nil then
+          LTitle.Text := '文字' + I.ToString;
+        LVerticalLayout.Visible := True;
+        LTileLayout.Add(LVerticalLayout);
+        OutputDebugString(PChar('+++++++++++++++++++++++++++++++'));
+      end;
+    end;
+  end else
   if LType.Equals(DUI_EVENT_CLICK) then
   begin
     if LCtlName.Equals(kclosebtn) then
@@ -89,5 +145,6 @@ begin
       Minimize;
   end;
 end;
+
 
 end.
