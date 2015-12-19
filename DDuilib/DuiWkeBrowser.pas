6d9367d1668c45f296e5c301b638178a23ed6823
ying32 <zying32@163.com>
2015-12-19 17:36:36 +0800

    完善了下wke内核浏览器支持，但js绑定方面总是无法获得js函数的参数，但可返回，很奇怪

--- DDuilib/DuiWkeBrowser.pas
+++ DDuilib/DuiWkeBrowser.pas
@@ -8,6 +8,9 @@
 //       版权所有 (C) 2015-2015 ying32 All Rights Reserved
 //
 //       c++ wke内核: https://github.com/BlzFans/wke
+//
+//       只封装了Delphi的方法,没有做Dui消息的封装
+//
 //***************************************************************************
 unit DuiWkeBrowser;
 
@@ -18,24 +21,104 @@ interface
 uses
   Windows,
   Types,
+  Classes,
   Duilib,
   DuiBase,
   Wke;
 
 type
 
-  TWkeWebbrowser = class(TDuiBase{$IFDEF SupportGeneric}<CNativeControlUI>{$ENDIF})
+  TTitleChangedEvent = procedure(Sender: TObject; const ATitle: string) of object;
+  TURLChangedEvent = procedure(Sender: TObject; const AURL: string) of object;
+  TPaintUpdatedEvent = procedure(Sender: TObject; DC: HDC; x, y, cx, cy: Integer) of object;
+  TAlertBoxEvent = procedure(Sender: TObject; const AMsg: string) of object;
+  TConfirmBoxEvent = procedure(Sender: TObject; const AMsg: string; var AResult: Boolean) of object;
+  TPromptBoxEvent = procedure(Sender: TObject; const AMsg, ADefaultResult, AResult: string; var AReturn: Boolean) of object;
+  TNavigationEvent = procedure(Sender: TObject; ANavigationType: wkeNavigationType; const AURL: string; var AResult: Boolean) of object;
+  TCreateViewEvent = procedure(Sender: TObject; ANavigationType: wkeNavigationType; const AURL: string; AWindowFeatures: PwkeWindowFeatures; var AResult: TwkeWebView) of object;
+  TDocumentReadyEvent = procedure(Sender: TObject) of object;
+  TLoadingFinishEvent = procedure(Sender: TObject; const AURL: string; AResult: wkeLoadingResult; const AFailedReason: string) of object;
+  TWindowClosingEvent = procedure(Sender: TObject; var AResult: Boolean) of object;
+  TConsoleMessageEvent= procedure(Sender: TObject; var AMessage: wkeConsoleMessage) of object;
+
+  TWkeWebbrowser = class
   private
     FWebView: TWkeWebView;
+    FNativeCtrl: CNativeControlUI;
+    FOnDocumentReady: TDocumentReadyEvent;
+    FOnLoadingFinish: TLoadingFinishEvent;
+    FOnNavigation: TNavigationEvent;
+    FOnWindowClosing: TWindowClosingEvent;
+    FOnCreateView: TCreateViewEvent;
+    FOnWindowDestroy: TNotifyEvent;
+    FOnPaintUpdated: TPaintUpdatedEvent;
+    FOnPromptBox: TPromptBoxEvent;
+    FOnTitleChanged: TTitleChangedEvent;
+    FOnAlertBox: TAlertBoxEvent;
+    FOnConfirmBox: TConfirmBoxEvent;
+    FOnURLChanged: TURLChangedEvent;
+    FWindowHandle: HWND;
+    FUserAgent: string;
+    FOnConsoleMessage: TConsoleMessageEvent;
+    procedure SetUserAgent(const Value: string);
   protected
-    procedure DoTitleChanged(webView: wkeWebView; ATitle: string); virtual;
-    procedure DoURLChanged(webView: wkeWebView; AURL: string); virtual;
+    procedure DoTitleChanged(const ATitle: string); virtual;
+    procedure DoURLChanged(const AURL: string); virtual;
+    procedure DoPaintUpdated(DC: HDC; x, y, cx, cy: Integer); virtual;
+    procedure DoAlertBox(const AMsg: string); virtual;
+    function DoConfirmBox(const AMsg: string): Boolean; virtual;
+    function DoPromptBox(const AMsg, ADefaultResult, AResult: string): Boolean; virtual;
+    function DoNavigation(ANavigationType: wkeNavigationType; const AURL: string): Boolean; virtual;
+    function DoCreateView(ANavigationType: wkeNavigationType; const AURL: string; AWindowFeatures: PwkeWindowFeatures): wkeWebView; virtual;
+    procedure DoDocumentReady; virtual;
+    procedure DoLoadingFinish(const AURL: string; AResult: wkeLoadingResult; const AFailedReason: string); virtual;
+    function DoWindowClosing: Boolean; virtual;
+    procedure DoWindowDestroy; virtual;
+    procedure DoConsoleMessage(var AMessage: wkeConsoleMessage); virtual;
   public
     destructor Destroy; override;
   public
-    procedure SetWkeWebbrowserUI(web: CNativeControlUI);
-    procedure FreeCpp;
-    procedure Navigate(const AURL: string);
+    procedure InitWkeWebBrowser(ANativeCtrl: CControlUI);
+    procedure Navigate(const AStr: string);
+    procedure Load(const AStr: string);
+    procedure LoadURL(const AURL: string);
+    procedure LoadFile(const AFileName: string);
+    procedure LoadHTML(const AHTML: string);
+    procedure StopLoading;
+    procedure Reload;
+    function CanGoBack: Boolean;
+    function GoBack: Boolean;
+    function CanGoForward: Boolean;
+    function GoForward: Boolean;
+    procedure EditorSelectAll;
+    procedure EditorCopy;
+    procedure EditorCut;
+    procedure EditorPaste;
+    procedure EditorDelete;
+    procedure SetFocus;
+    procedure KillFocus;
+    function RunJS(const AScript: string): jsValue;
+    function GlobalExec: jsExecState;
+    procedure Sleep;
+    procedure Wake;
+    function IsAwake: Boolean;
+  public
+    property WebView: TWkeWebView read FWebView;
+    property WindowHandle: HWND read FWindowHandle;
+    property UserAgent: string read FUserAgent write SetUserAgent;
+    property OnTitleChanged: TTitleChangedEvent read FOnTitleChanged write FOnTitleChanged;
+    property OnURLChanged: TURLChangedEvent read FOnURLChanged write FOnURLChanged;
+    property OnPaintUpdated: TPaintUpdatedEvent read FOnPaintUpdated write FOnPaintUpdated;
+    property OnAlertBox: TAlertBoxEvent read FOnAlertBox write FOnAlertBox;
+    property OnConfirmBox: TConfirmBoxEvent read FOnConfirmBox write FOnConfirmBox;
+    property OnPromptBox: TPromptBoxEvent read FOnPromptBox write FOnPromptBox;
+    property OnNavigation: TNavigationEvent read FOnNavigation write FOnNavigation;
+    property OnCreateView: TCreateViewEvent read FOnCreateView write FOnCreateView;
+    property OnDocumentReady: TDocumentReadyEvent read FOnDocumentReady write FOnDocumentReady;
+    property OnLoadingFinish: TLoadingFinishEvent read FOnLoadingFinish write FOnLoadingFinish;
+    property OnWindowClosing: TWindowClosingEvent read FOnWindowClosing write FOnWindowClosing;
+    property OnWindowDestroy: TNotifyEvent read FOnWindowDestroy write FOnWindowDestroy;
+    property OnConsoleMessage: TConsoleMessageEvent read FOnConsoleMessage write FOnConsoleMessage;
   end;
 
 implementation
@@ -43,69 +126,358 @@ implementation
 
 procedure OnwkeTitleChangedCallback(webView: wkeWebView; param: Pointer; title: wkeString); cdecl;
 begin
-  TWkeWebbrowser(param).DoTitleChanged(webView,
-    {$IFDEF UNICODE}webView.GetStringW{$ELSE}webView.GetString{$ENDIF}(title));
+  TWkeWebbrowser(param).DoTitleChanged(webView.GetString(title));
 end;
 
 procedure OnwkeURLChangedCallback(webView: wkeWebView; param: Pointer; url: wkeString); cdecl;
 begin
-  TWkeWebbrowser(param).DoURLChanged(webView,
-    {$IFDEF UNICODE}webView.GetStringW{$ELSE}webView.GetString{$ENDIF}(url));
+  TWkeWebbrowser(param).DoURLChanged(webView.GetString(url));
 end;
+
+procedure OnwkePaintUpdatedCallback(webView: wkeWebView; param: Pointer; DC: HDC;
+   x: Integer; y: Integer; cx: Integer; cy: Integer); cdecl;
+begin
+  TWkeWebbrowser(param).DoPaintUpdated(DC, x, y, cx, cy);
+end;
+
+procedure OnwkeAlertBoxCallback(webView: wkeWebView; param: Pointer; msg: wkeString); cdecl;
+begin
+  TWkeWebbrowser(param).DoAlertBox(webView.GetString(msg));
+end;
+
+function OnwkeConfirmBoxCallback(webView: wkeWebView; param: Pointer; msg: wkeString): Boolean; cdecl;
+begin
+  Result := TWkeWebbrowser(param).DoConfirmBox(webView.GetString(msg));
+end;
+
+function OnwkePromptBoxCallback(webView: wkeWebView; param: Pointer; msg: wkeString;
+  defaultResult: wkeString; AResult: wkeString): Boolean; cdecl;
+begin
+  Result := TWkeWebbrowser(param).DoPromptBox(webView.GetString(msg),
+    webView.GetString(defaultResult), webView.GetString(AResult));
+end;
+
+function OnwkeNavigationCallback(webView: wkeWebView; param: Pointer;
+  navigationType: wkeNavigationType; url: wkeString): Boolean; cdecl;
+begin
+  Result := TWkeWebbrowser(param).DoNavigation(navigationType, webView.GetString(url));
+end;
+
+function OnwkeCreateViewCallback(webView: wkeWebView; param: Pointer; navigationType: wkeNavigationType;
+   url: wkeString; windowFeatures: PwkeWindowFeatures): wkeWebView; cdecl;
+begin
+  Result := TWkeWebbrowser(param).DoCreateView(navigationType, webView.GetString(url), windowFeatures);
+end;
+
+procedure OnwkeDocumentReadyCallback(webView: wkeWebView; param: Pointer); cdecl;
+begin
+  TWkeWebbrowser(param).DoDocumentReady;
+end;
+
+procedure OnwkeLoadingFinishCallback(webView: wkeWebView; param: Pointer; url: wkeString;
+  result: wkeLoadingResult; failedReason: wkeString); cdecl;
+begin
+  TWkeWebbrowser(param).DoLoadingFinish(webView.GetString(url),
+    result, webView.GetString(failedReason));
+end;
+
+function OnwkeWindowClosingCallback(webWindow: wkeWebView; param: Pointer): Boolean; cdecl;
+begin
+  Result := TWkeWebbrowser(param).DoWindowClosing;
+end;
+
+procedure OnwkeWindowDestroyCallback(webWindow: wkeWebView; param: Pointer); cdecl;
+begin
+  TWkeWebbrowser(param).DoWindowDestroy;
+end;
+
+procedure OnwkeConsoleMessageCallback(webView: wkeWebView; param: Pointer; var AMessage: wkeConsoleMessage); cdecl;
+begin
+  TWkeWebbrowser(param).DoConsoleMessage(AMessage);
+end;
+
 
 { TWkeWebbrowser }
 
 destructor TWkeWebbrowser.Destroy;
 begin
-  if FWebView <> nil then
+  // 之所以用检测下FWindowHandle是因为主窗口释放时，这也被释放掉了
+  if (FWebView <> nil) and IsWindow(FWindowHandle) then
     FWebView.DestroyWebWindow;
   inherited;
 end;
 
-procedure TWkeWebbrowser.FreeCpp;
+procedure TWkeWebbrowser.Navigate(const AStr: string);
 begin
-  if FThis <> nil then
+  Load(AStr);
+end;
+
+procedure TWkeWebbrowser.Reload;
+begin
+  if FWebView <> nil then
+    FWebView.Reload;
+end;
+
+function TWkeWebbrowser.RunJS(const AScript: string): jsValue;
+begin
+  Result := 0;
+  if FWebView <> nil then
+    Result := FWebView.RunJS(AScript);
+end;
+
+procedure TWkeWebbrowser.SetFocus;
+begin
+  if FWebView <> nil then
+    FWebView.SetFocus;
+end;
+
+procedure TWkeWebbrowser.SetUserAgent(const Value: string);
+begin
+  if FUserAgent <> Value then
   begin
-    FThis.CppDestroy;
-    FThis := nil;
+    if Assigned(FWebView) then
+    begin
+      FUserAgent := Value;
+      FWebView.UserAgent := FUserAgent;
+    end;
   end;
 end;
 
-procedure TWkeWebbrowser.Navigate(const AURL: string);
+procedure TWkeWebbrowser.Sleep;
 begin
   if FWebView <> nil then
-  {$IFDEF UNICODE}
-    FWebView.loadURLW(PChar(AURL));
-  {$ELSE}
-    FWebView.loadURLA(PChar(AURL));
-  {$ENDIF}
+    FWebView.Sleep;
 end;
 
-procedure TWkeWebbrowser.SetWkeWebbrowserUI(web: CNativeControlUI);
+procedure TWkeWebbrowser.StopLoading;
+begin
+  if FWebView <> nil then
+    FWebView.StopLoading;
+end;
+
+procedure TWkeWebbrowser.Wake;
+begin
+  if FWebView <> nil then
+    FWebView.Wake;
+end;
+
+procedure TWkeWebbrowser.DoAlertBox(const AMsg: string);
+begin
+  if Assigned(FOnAlertBox) then
+    FOnAlertBox(Self, AMsg);
+end;
+
+function TWkeWebbrowser.DoConfirmBox(const AMsg: string): Boolean;
+begin
+  Result := True;
+  if Assigned(FOnConfirmBox) then
+    FOnConfirmBox(Self, AMsg, Result);
+end;
+
+procedure TWkeWebbrowser.DoConsoleMessage(var AMessage: wkeConsoleMessage);
+begin
+  if Assigned(FOnConsoleMessage) then
+    FOnConsoleMessage(Self, AMessage);
+end;
+
+function TWkeWebbrowser.DoCreateView(ANavigationType: wkeNavigationType;
+  const AURL: string; AWindowFeatures: PwkeWindowFeatures): wkeWebView;
+begin
+  Result := FWebView;
+  if Assigned(FOnCreateView) then
+    FOnCreateView(Self, ANavigationType, AURL, AWindowFeatures, Result);
+end;
+
+procedure TWkeWebbrowser.DoDocumentReady;
+begin
+  if Assigned(FOnDocumentReady) then
+    FOnDocumentReady(Self);
+end;
+
+procedure TWkeWebbrowser.DoLoadingFinish(const AURL: string;
+  AResult: wkeLoadingResult; const AFailedReason: string);
+begin
+  if Assigned(FOnLoadingFinish) then
+    FOnLoadingFinish(Self, AURL, AResult, AFailedReason);
+end;
+
+function TWkeWebbrowser.DoNavigation(ANavigationType: wkeNavigationType;
+  const AURL: string): Boolean;
+begin
+  Result := True;
+  if Assigned(FOnNavigation) then
+    FOnNavigation(Self, ANavigationType, AURL, Result);
+end;
+
+procedure TWkeWebbrowser.DoPaintUpdated(DC: HDC; x, y, cx, cy: Integer);
+begin
+  if Assigned(FOnPaintUpdated) then
+    FOnPaintUpdated(Self, DC, x, y, cx, cy);
+end;
+
+function TWkeWebbrowser.DoPromptBox(const AMsg, ADefaultResult,
+   AResult: string): Boolean;
+begin
+  Result := True;
+  if Assigned(FOnPromptBox) then
+    FOnPromptBox(Self, AMsg, ADefaultResult, AResult, Result);
+end;
+
+procedure TWkeWebbrowser.DoTitleChanged(const ATitle: string);
+begin
+  if Assigned(FOnTitleChanged) then
+    FOnTitleChanged(Self, ATitle);
+end;
+
+procedure TWkeWebbrowser.DoURLChanged(const AURL: string);
+begin
+  if Assigned(FOnURLChanged) then
+    FOnURLChanged(Self, AURL);
+end;
+
+function TWkeWebbrowser.DoWindowClosing: Boolean;
+begin
+  Result := True;
+  if Assigned(FOnWindowClosing) then
+    FOnWindowClosing(Self, Result);
+end;
+
+procedure TWkeWebbrowser.DoWindowDestroy;
+begin
+  if Assigned(FOnWindowDestroy) then
+    FOnWindowDestroy(Self);
+end;
+
+function TWkeWebbrowser.CanGoBack: Boolean;
+begin
+  Result := False;
+  if FWebView <> nil then
+    FWebView.CanGoBack;
+end;
+
+function TWkeWebbrowser.CanGoForward: Boolean;
+begin
+  Result := False;
+  if FWebView <> nil then
+    FWebView.CanGoForward;
+end;
+
+procedure TWkeWebbrowser.EditorCopy;
+begin
+  if FWebView <> nil then
+    FWebView.EditorCopy;
+end;
+
+procedure TWkeWebbrowser.EditorCut;
+begin
+  if FWebView <> nil then
+    FWebView.EditorCut;
+end;
+
+procedure TWkeWebbrowser.EditorDelete;
+begin
+  if FWebView <> nil then
+    FWebView.EditorDelete;
+end;
+
+procedure TWkeWebbrowser.EditorPaste;
+begin
+  if FWebView <> nil then
+    FWebView.EditorPaste;
+end;
+
+procedure TWkeWebbrowser.EditorSelectAll;
+begin
+  if FWebView <> nil then
+    FWebView.EditorSelectAll;
+end;
+
+function TWkeWebbrowser.GlobalExec: jsExecState;
+begin
+  Result := nil;
+  if FWebView <> nil then
+    Result := FWebView.GlobalExec;
+end;
+
+function TWkeWebbrowser.GoBack: Boolean;
+begin
+  Result := False;
+  if FWebView <> nil then
+    FWebView.GoBack;
+end;
+
+function TWkeWebbrowser.GoForward: Boolean;
+begin
+  Result := False;
+  if FWebView <> nil then
+    FWebView.GoForward;
+end;
+
+procedure TWkeWebbrowser.InitWkeWebBrowser(ANativeCtrl: CControlUI);
 var
   R: TRect;
 begin
-  if (web <> nil) and (web <> FThis) then
+  if (ANativeCtrl <> nil) and (ANativeCtrl <> FNativeCtrl) then
   begin
-    FThis := web;
-    FWebView := wkeCreateWebWindow(WKE_WINDOW_TYPE_CONTROL, web.GetManager.GetPaintWindow, 0, 0, 1, 1);
-    FWebView.OnTitleChanged(OnwkeTitleChangedCallback, Self);
-    FWebView.OnURLChanged(OnwkeURLChangedCallback, Self);
-    FThis.SetNativeHandle(FWebView.GetWindowHandle);
-    R := web.GetPos;
+    FNativeCtrl := CNativeControlUI(ANativeCtrl);
+    FWebView := wkeCreateWebWindow(WKE_WINDOW_TYPE_CONTROL, FNativeCtrl.GetManager.GetPaintWindow, 0, 0, 1, 1);
+    FWindowHandle := FWebView.WindowHandle;
+    FWebView.SetOnTitleChanged(OnwkeTitleChangedCallback, Self);
+    FWebView.SetOnURLChanged(OnwkeURLChangedCallback, Self);
+    FWebView.SetOnPaintUpdated(OnwkePaintUpdatedCallback, Self);
+    FWebView.SetOnAlertBox(OnwkeAlertBoxCallback, Self);
+    FWebView.SetOnConfirmBox(OnwkeConfirmBoxCallback, Self);
+    FWebView.SetOnPromptBox(OnwkePromptBoxCallback, Self);
+    FWebView.SetOnNavigation(OnwkeNavigationCallback, Self);
+    FWebView.SetOnCreateView(OnwkeCreateViewCallback, Self);
+    FWebView.SetOnLoadingFinish(OnwkeLoadingFinishCallback, Self);
+    FWebView.SetOnWindowClosing(OnwkeWindowClosingCallback, Self);
+    FWebView.SetOnWindowDestroy(OnwkeWindowDestroyCallback, Self);
+    FWebView.SetOnDocumentReady(OnwkeDocumentReadyCallback, Self);
+    FWebView.SetOnConsoleMessage(OnwkeConsoleMessageCallback, Self);
+
+    FNativeCtrl.SetNativeHandle(FWebView.WindowHandle);
+    R := FNativeCtrl.GetPos;
     FWebView.MoveWindow(R.Left, R.Top, R.Right - R.Left, R.Bottom - R.Top);
     FWebView.ShowWindow(True);
   end;
 end;
 
-procedure TWkeWebbrowser.DoTitleChanged(webView: wkeWebView; ATitle: string);
+function TWkeWebbrowser.IsAwake: Boolean;
 begin
-
+  Result := False;
+  if FWebView <> nil then
+    FWebView.IsAwake;
 end;
 
-procedure TWkeWebbrowser.DoURLChanged(webView: wkeWebView; AURL: string);
+procedure TWkeWebbrowser.KillFocus;
 begin
+  if FWebView <> nil then
+    FWebView.KillFocus;
+end;
 
+procedure TWkeWebbrowser.Load(const AStr: string);
+begin
+  if Assigned(FWebView) then
+    FWebView.Load(AStr);
+end;
+
+procedure TWkeWebbrowser.LoadFile(const AFileName: string);
+begin
+  if Assigned(FWebView) then
+    FWebView.LoadFile(AFileName);
+end;
+
+procedure TWkeWebbrowser.LoadHTML(const AHTML: string);
+begin
+  if Assigned(FWebView) then
+    FWebView.LoadHTML(AHTML);
+end;
+
+procedure TWkeWebbrowser.LoadURL(const AURL: string);
+begin
+  if Assigned(FWebView) then
+    FWebView.LoadURL(AURL);
 end;
 
 initialization
--- Demo/WkeBrowser/WkeBrowser.dpr
+++ Demo/WkeBrowser/WkeBrowser.dpr
@@ -12,12 +12,16 @@ uses
   Duilib,
   DuiConst,
   DuiWindowImplBase,
+  Wke,
   DuiWkeBrowser;
 
 type
   TWkeBrowserWindow = class(TDuiWindowImplBase)
   private
     FWkeWebbrowser: TWkeWebbrowser;
+    procedure OnTitleChanged(Sender: TObject; const ATitle: string);
+    procedure OnURLChanged(Sender: TObject; const AURL: string);
+    procedure OnDocumentReady(Sender: TObject);
   protected
     procedure DoInitWindow; override;
     procedure DoNotify(var Msg: TNotifyUI); override;
@@ -30,11 +34,31 @@ type
 
 { TWkeBrowserWindow }
 
+function JsMessageBox(es: jsExecState): JsValue;
+begin
+  Writeln(jsArgCount(es));
+  MessageBox(0, PChar(JScript.ToTempString(es, JScript.Arg(es, 0))),
+                PChar(JScript.ToTempString(es, JScript.Arg(es, 1))),
+                0);
+  Result := JScript.String_(es, '这是一个返回值测试');
+end;
+
+function JsFunc2(es: jsExecState): jsValue;
+begin
+  Result := JScript.Arg(es, 0);
+end;
+
 constructor TWkeBrowserWindow.Create;
 begin
   inherited Create('WkebrowserWindow.xml', 'skin');
   FWkeWebbrowser := TWkeWebbrowser.Create;
+  FWkeWebbrowser.OnTitleChanged := OnTitleChanged;
+  FWkeWebbrowser.OnURLChanged := OnURLChanged;
+  FWkeWebbrowser.OnDocumentReady := OnDocumentReady;
   CreateWindow(0, 'wke浏览器', UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE);
+
+  JScript.BindFunction('jsMessageBox', JsMessageBox, 2);
+  JScript.BindFunction('JsFunc2', JsFunc2, 1);
 end;
 
 destructor TWkeBrowserWindow.Destroy;
@@ -58,16 +82,17 @@ begin
 end;
 
 procedure TWkeBrowserWindow.DoInitWindow;
-var
-  pControl: CNativeControlUI;
 begin
   inherited;
-  pControl := CNativeControlUI(FindControl('webbrowser'));
-  if pControl <> nil then
-  begin
-    FWkeWebbrowser.SetWkeWebbrowserUI(pControl);
-    FWkeWebbrowser.Navigate('http://www.baidu.com');
-  end;
+
+  FWkeWebbrowser.InitWkeWebBrowser(FindControl('webbrowser'));
+  FWkeWebbrowser.UserAgent := 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36';
+
+  //FWkeWebbrowser.Navigate('http://www.baidu.com');
+//  FWkeWebbrowser.Navigate('http://www.oschina.net');
+  FWkeWebbrowser.LoadFile('skin\testhtml.html');
+
+
 end;
 
 procedure TWkeBrowserWindow.DoNotify(var Msg: TNotifyUI);
@@ -80,17 +105,35 @@ begin
   end;
 end;
 
+procedure TWkeBrowserWindow.OnDocumentReady(Sender: TObject);
+begin
+  Writeln('加载完成');
+end;
+
+procedure TWkeBrowserWindow.OnTitleChanged(Sender: TObject;
+  const ATitle: string);
+begin
+  Writeln('title=', ATitle);
+end;
+
+procedure TWkeBrowserWindow.OnURLChanged(Sender: TObject; const AURL: string);
+begin
+  Writeln('url=', AURL);
+end;
+
 var
   WkeBrowserWindow: TWkeBrowserWindow;
 
 begin
   try
+
     DuiApplication.Initialize;
     WkeBrowserWindow := TWkeBrowserWindow.Create;
     WkeBrowserWindow.CenterWindow;
     WkeBrowserWindow.Show;
     DuiApplication.Run;
     WkeBrowserWindow.Free;
+
   except
     on E: Exception do
       Writeln(E.Message);
