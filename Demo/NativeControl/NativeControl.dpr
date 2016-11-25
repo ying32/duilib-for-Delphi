ec3f45c1d517f9145516c160c8a3248beea9d6b0
ying32 <zying32@163.com>
2016-11-25 15:42:58 +0800

    原生控件用来测试vcl，发现有些消息无法收到。。。暂未去深究

--- Demo/NativeControl/NativeControl.dpr
+++ Demo/NativeControl/NativeControl.dpr
@@ -2,6 +2,8 @@ program NativeControl;
 
 {$APPTYPE CONSOLE}
 
+{$I DDuilib.inc}
+
 {$R *.res}
 
 uses
@@ -12,13 +14,28 @@ uses
   StdCtrls,
   Duilib,
   DuiConst,
+  ComCtrls,
+  Forms,
   DuiWindowImplBase;
 
 type
   TDuiNativeControlTest = class(TDuiWindowImplBase)
   private
     FButton: TButton;
+    FTabs: CTabLayoutUI;
+    FTabTool: CTabLayoutUI;
+    FVclPageCtl: CNativeControlUI;
+    FVclListViewCtl: CNativeControlUI;
+    FWCaption: CLabelUI;
+
+    FListV: TListView;
+    FPages: TPageControl;
+
+
     procedure ButtonClick(Sender: TObject);
+    procedure OnOptionSelectchanged(Msg: TNotifyUI);
+    procedure CreateVclControls;
+    procedure CreateVclSubControls;
   protected
     procedure DoInitWindow; override;
     procedure DoNotify(var Msg: TNotifyUI); override;
@@ -37,17 +54,86 @@ end;
 
 constructor TDuiNativeControlTest.Create;
 begin
+  CreateVclControls;
+  //inherited Create('NativeControlTest.xml', 'skin');
+  inherited Create('dlg_main2.xml', 'skin\PCManager\');
+  CreateDuiWindow(0, 'NativeControl测试');
+end;
+
+procedure TDuiNativeControlTest.CreateVclControls;
+var
+  LCol: TListColumn;
+begin
   // 有点奇怪，delphi7下放到create与CreateDuiWindow之间就报错，高版本则无此问题
   FButton := TButton.Create(nil);
   FButton.Caption := '按钮1';
   FButton.OnClick := ButtonClick;
-  inherited Create('NativeControlTest.xml', 'skin');
-  CreateDuiWindow(0, 'NativeControl测试');
+
+  FPages := TPageControl.Create(nil);
+  FPages.Left := 0 - (FPages.Width + 1);
+
+
+  FListV := TListView.Create(nil);
+  // 不使用visible属性
+  FListV.Left := 0 - (FListV.Width + 1);
+
+  FListV.GridLines := True;
+  FListV.RowSelect := True;
+  FListV.ViewStyle := vsReport;
+
+  LCol := FListV.Columns.Add;
+  LCol.Caption := '第一列';
+  LCol.Width := 100;
+
+  LCol := FListV.Columns.Add;
+  LCol.Caption := '第二列';
+  LCol.Width := 100;
+
+  LCol := FListV.Columns.Add;
+  LCol.Caption := '第三列';
+  LCol.Width := 100;
+
+end;
+
+procedure TDuiNativeControlTest.CreateVclSubControls;
+var
+  LTabSheet: TTabSheet;
+  LItem: TListItem;
+  I: Integer;
+  LButton: TButton;
+begin
+  // 添加了无法切换tab，不响应某些消息了。。。。估计是和vcl和冲突，某些消息需要转发
+  LTabSheet := TTabSheet.Create(FPages);
+  LTabSheet.Parent := FPages;
+  LTabSheet.PageControl := FPages;
+  LTabSheet.Caption := '第一页';
+
+  LButton := TButton.Create(LTabSheet);
+  LButton.Parent := LTabSheet;
+  LButton.Caption := '按钮';
+
+  LTabSheet := TTabSheet.Create(FPages);
+  LTabSheet.Parent := FPages;
+  LTabSheet.PageControl := FPages;
+  LTabSheet.Caption := '第二页';
+  // 添加了文字无法显示。。。。估计是和vcl和冲突，某些消息需要转发
+  FListV.Items.BeginUpdate;
+  try
+    for I := 0 to 100 do
+    begin
+      LItem := FListV.Items.Add;
+      LItem.Caption := IntToStr(I+1);
+      LItem.SubItems.Add('1111');
+    end;
+  finally
+    FListV.Items.EndUpdate;
+  end;
 end;
 
 destructor TDuiNativeControlTest.Destroy;
 begin
-  FButton.Free;
+  FreeAndNil(FListV);
+  FreeAndNil(FButton);
   inherited;
 end;
 
@@ -68,21 +154,70 @@ var
   LNativeCtl: CNativeControlUI;
 begin
   inherited;
-  LNativeCtl := CNativeControlUI(FindControl('test1'));
-  if LNativeCtl <> nil then
+{$IFDEF SupportGeneric}
+  FTabs := FindControl<CTabLayoutUI>('tabs');
+  FTabTool := FindControl<CTabLayoutUI>('tabtool');
+  FVclPageCtl := FindControl<CNativeControlUI>('vclpagectl');
+  FWCaption := FindControl<CLabelUI>('lblCaption');
+  FVclListViewCtl := FindControl<CNativeControlUI>('vcllistviewctl');
+{$ELSE}
+  FTabs := CTabLayoutUI(FindControl('tabs'));
+  FTabTool := CTabLayoutUI(FindControl('tabtool'));
+  FVclPageCtl := CNativeControlUI(FindControl('vclpagectl'));
+  FWCaption := CLabelUI(FindControl('lblCaption'));
+  FVclListViewCtl := CNativeControlUI(FindControl('vcllistviewctl'));
+{$ENDIF}
+  FWCaption.Text := 'VCL控件测试';
+
+  if FVclPageCtl <> nil then
   begin
-    // 这个只能使用delphi设置parent
-    FButton.ParentWindow := Handle;
-    LNativeCtl.SetNativeHandle(FButton.Handle);
+    FPages.ParentWindow := Handle;
+    FVclPageCtl.SetNativeHandle(FPages.Handle);
   end;
+  if FVclListViewCtl <> nil then
+  begin
+    FListV.ParentWindow := Handle;
+    FVclListViewCtl.SetNativeHandle(FListV.Handle);
+  end;
+
+  CreateVclSubControls;
+
+//  LNativeCtl := CNativeControlUI(FindControl('test1'));
+//  if LNativeCtl <> nil then
+//  begin
+//    // 这个只能使用delphi设置parent
+//    FButton.ParentWindow := Handle;
+//    LNativeCtl.SetNativeHandle(FButton.Handle);
+//  end;
 end;
 
 procedure TDuiNativeControlTest.DoNotify(var Msg: TNotifyUI);
 begin
+  if Msg.sType.m_pStr = DUI_MSGTYPE_SELECTCHANGED then
+    OnOptionSelectchanged(Msg);
   Writeln(string(Msg.sType.m_pstr));
   inherited;
 end;
 
+
+procedure TDuiNativeControlTest.OnOptionSelectchanged(Msg: TNotifyUI);
+var
+  LName: string;
+  LIndex: Integer;
+begin
+  LName := Msg.pSender.Name;
+  if Copy(LName, 1, 6) = 'tabsel' then
+  begin
+    LIndex := StrToIntDef(Copy(LName, 7, 1), -1) - 1;
+    if LIndex >= 0 then
+      FTabs.SelectIndex := LIndex;
+  end else if Copy(LName, 1, 7) = 'toolsel' then
+  begin
+    LIndex := StrToIntDef(Copy(LName, 8, 1), -1) - 1;
+    if LIndex >= 0 then
+      FTabTool.SelectIndex := LIndex;
+  end;
+end;
 
 var
   DuiNativeControlTest: TDuiNativeControlTest;
